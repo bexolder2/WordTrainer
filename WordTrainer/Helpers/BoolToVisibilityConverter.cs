6f21fff2b748d28c645aae784192d2ec22f7b840
bexolder2 <dimich2013@yahoo.com>
2022-09-10 17:17:20 +0300

    Added bool to visibility, visibility invertor converters

---
+++ WordTrainer/Helpers/BoolToVisibilityConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WordTrainer.Helpers
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility visibility = Visibility.Collapsed;
+            if (value != null && value is bool val)
+            {
+                visibility = val ? Visibility.Visible : Visibility.Collapsed;
+                if (parameter != null && parameter.ToString() == "invert")
+                {
+                    visibility = val ? Visibility.Collapsed : Visibility.Visible;
+                }
+            }
+
+            return visibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool val = false;
+            if (value != null && value is Visibility visibility)
+            {
+                val = visibility == Visibility.Visible ? true : false;
+                if (parameter != null && parameter.ToString() == "invert")
+                {
+                    val = visibility == Visibility.Visible ? false : true;
+                }
+            }
+
+            return val;
+        }
+    }
+}
---
+++ WordTrainer/Helpers/VisibilityInvertorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WordTrainer.Helpers
+{
+    public class VisibilityInvertorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Visibility result = Visibility.Collapsed;
+            if (value != null && value is Visibility)
+            {
+                result = (Visibility)value == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
