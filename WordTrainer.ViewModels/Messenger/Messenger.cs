14d3d3f19eb9451c7c714cc77f71b326b97f85c5
bexolder2 <dimich2013@yahoo.com>
2022-09-17 20:23:47 +0300

    Implemented Messenger

---
+++ WordTrainer.ViewModels/Messenger/Messenger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WordTrainer.Models;
+using WordTrainer.Models.Interfaces;
+
+namespace WordTrainer.ViewModels
+{
+    public class Messenger
+    {
+        #region Singletone
+        private static Lazy<Messenger> instance = new();
+
+        public static Messenger Instance => instance.Value;
+        #endregion
+
+        private List<MessangerItem> subscribers;
+
+        public Messenger() 
+        {
+            subscribers = new List<MessangerItem>();
+        }
+
+        public void Register<T>(IViewModel vm, Action<object> handler)
+        {
+            subscribers.Add(new MessangerItem(typeof(T), vm, handler));
+        }
+
+        public void Send<T>(T data)
+        {
+            var subscribers_ = subscribers.FindAll(x => x.Type == data?.GetType());
+            if (subscribers_.Any())
+            {
+                subscribers_.ForEach((item) => {
+                    item.Handler.Invoke(data);
+                });
+            }
+        }
+    }
+}
